a5566fcd807af83053a28bba053ea116c326d2fb
RobertsPC <RobertsPC@DESKTOP-QKJ3HFP>
2022-10-31 09:03:00 -0500

    Initial upload

--- RequisitionForm/Properties/AssemblyInfo.cs
+++ RequisitionForm/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("RequisitionForm")]
+[assembly: AssemblyTitle("Requisition Form Tool")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("RequisitionForm")]
-[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyProduct("Requisition Form Tool")]
+[assembly: AssemblyCopyright("Copyright © Robert Woemmel 2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
